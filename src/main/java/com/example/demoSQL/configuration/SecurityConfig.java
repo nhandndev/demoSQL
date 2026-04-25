771da12f3527e58f39db1ca16c3cdb20c2ea5459
LilNhan <nhandn.dev@gmail.com>
2026-04-25 11:50:14 +0700

    Update SecurityConfig.java

--- src/main/java/com/example/demoSQL/configuration/SecurityConfig.java
+++ src/main/java/com/example/demoSQL/configuration/SecurityConfig.java
@@ -1,19 +1,51 @@
 package com.example.demoSQL.configuration;
 
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.spec.SecretKeySpec;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    private static final String[] PUBLIC_ENDPOINTS = {
+            "/user",
+            "/auth/token",
+            "/login",
+            "/auth/introspect"
+    };
+    @NonFinal
+    @Value("${jwt.signerKey")
+    protected String SIGNER_KEY;
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
-        return http.build();
+    public JwtDecoder jwtDecoder() {
+        SecretKeySpec secretKey = new SecretKeySpec(SIGNER_KEY.getBytes(),"HmacSHA256");
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request ->
+                request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS ).permitAll()
+                        .anyRequest().authenticated());
+        httpSecurity.csrf(csrf ->csrf.disable());
+        httpSecurity.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
+        );
+        return httpSecurity.build();
+    }
+
+
 }
