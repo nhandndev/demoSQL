93e9cd70912697a6d16c3b4a6999bb0d7d7c5b09
LilNhan <nhandn.dev@gmail.com>
2026-04-29 17:33:51 +0700

    Update ErrorCode.java

--- src/main/java/com/example/demoSQL/globalexceptionhandler/ErrorCode.java
+++ src/main/java/com/example/demoSQL/globalexceptionhandler/ErrorCode.java
@@ -1,32 +1,105 @@
 package com.example.demoSQL.globalexceptionhandler;
 
-public enum ErrorCode {
-    KEY_INVALID(0001,"KEY_INVALID"),
-    USER_EXISTED(1001,"USER_EXISTED"),
-    USER_NOT_EXISTED(1002,"USER_NOT_EXISTED"),
-    USER_DISABLED(1003,"USER_DISABLED"),
-    MISSING_REQUIRED(1004,"MISSING_REQUIRED"),
-    USERNAME_INVALID(1005,"USERNAME_INVALID"),
-    PASSWORD_INVALID(1006,"PASSWORD_INVALID"),
-    EMAIL_INVALID(1007,"EMAIL_INVALID"),
-    NAME_EXISTED(1007,"NAME_EXISTED"),
-    UNAUTHORIZED(1008,"UNAUTHORIZED"),
-    TOKEN_CANNOT_CREATE(1009,"TOKEN_CANNOT_CREATE"),
-    TOKEN_INVALID(1010,"TOKEN_INVALID"),
-    UNCATEGORIZED_EXITED(9999,"UNCATEGORIZED EXCEPTION")
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
 
-    ;
-    private int code;
-    private String message;
-    private ErrorCode(int code, String message) {
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public enum ErrorCode {
+
+    KEY_INVALID(
+            1000,
+            "KEY_INVALID",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    USER_EXISTED(
+            1001,
+            "USER_EXISTED",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    USER_NOT_EXISTED(
+            1002,
+            "USER_NOT_EXISTED",
+            HttpStatus.NOT_FOUND
+    ),
+
+    USER_DISABLED(
+            1003,
+            "USER_DISABLED",
+            HttpStatus.FORBIDDEN
+    ),
+
+    MISSING_REQUIRED(
+            1004,
+            "MISSING_REQUIRED",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    USERNAME_INVALID(
+            1005,
+            "USERNAME_INVALID",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    PASSWORD_INVALID(
+            1006,
+            "PASSWORD_INVALID",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    EMAIL_INVALID(
+            1007,
+            "EMAIL_INVALID",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    NAME_EXISTED(
+            1008,
+            "NAME_EXISTED",
+            HttpStatus.BAD_REQUEST
+    ),
+
+    UNAUTHENTICATED(
+            1009,
+            "UNAUTHENTICATED",
+            HttpStatus.UNAUTHORIZED
+    ),
+
+    UNAUTHORIZED(
+            1010,
+            "UNAUTHORIZED",
+            HttpStatus.FORBIDDEN
+    ),
+
+    TOKEN_CANNOT_CREATE(
+            1011,
+            "TOKEN_CANNOT_CREATE",
+            HttpStatus.INTERNAL_SERVER_ERROR
+    ),
+
+    TOKEN_INVALID(
+            1012,
+            "TOKEN_INVALID",
+            HttpStatus.UNAUTHORIZED
+    ),
+
+    UNCATEGORIZED_EXCEPTION(
+            9999,
+            "UNCATEGORIZED_EXCEPTION",
+            HttpStatus.INTERNAL_SERVER_ERROR
+    );
+
+    int code;
+    String message;
+    HttpStatus httpStatus;
+
+    ErrorCode(int code, String message, HttpStatus httpStatus) {
         this.code = code;
         this.message = message;
-    }
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
+        this.httpStatus = httpStatus;
     }
 }
