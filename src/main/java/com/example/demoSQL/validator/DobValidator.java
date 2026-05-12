b3f73e3c4732c89b6a4ca73efc6236bb61073f0c
LilNhan <nhandn.dev@gmail.com>
2026-05-12 16:37:37 +0700

    Update DobValidator.java

--- src/main/java/com/example/demoSQL/validator/DobValidator.java
+++ src/main/java/com/example/demoSQL/validator/DobValidator.java
@@ -7,16 +7,18 @@ import jakarta.validation.ConstraintValidatorContext;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
 public class DobValidator implements ConstraintValidator<DobConstraint, LocalDate> {
     private int min;
     @Override
     public void initialize(DobConstraint constraintAnnotation) {
-    this.min = constraintAnnotation.min();
+        ConstraintValidator.super.initialize(constraintAnnotation);
+        this.min = constraintAnnotation.min();
     }
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
-        if (value == null) {
+        if (Objects.isNull(value)) {
             return true;
         }
        long years = ChronoUnit.YEARS.between(value, LocalDate.now());
